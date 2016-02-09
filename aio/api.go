e5c7258ea7f74e2bccc5db773dd4e815de8a6c00
Dave Hulihan <dhulihan@gmail.com>
2016-02-09 14:13:01 -0700

    type assert on json interface{}

--- aio/api.go
+++ aio/api.go
@@ -7,11 +7,28 @@ import (
 	"io/ioutil"
 	"fmt"
 	"bytes"
+	"encoding/json"
+	"reflect"
+	"time"
 )
 
 const (
  	api_url_base = "https://io.adafruit.com/api"
 )
+
+type Feed struct {
+	id string
+	name string
+	key string
+	description string
+	unit_type string
+	unit_symbol string
+	status string
+	visibility string
+	enabled string
+	created_at time.Time
+	updated_at time.Time
+}
 
 func Feeds() []string {	
 	url := api_url_base + "/feeds"
@@ -29,14 +46,32 @@ func Feeds() []string {
 	} else {
 		// close Body after everything is done
 		defer resp.Body.Close()
-		contents, err := ioutil.ReadAll(resp.Body)
+		b, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			log.Printf("%s", err)
 			os.Exit(1)
 		}
-		// return string[]{contents}
-		feeds := []string {string(contents)}
-		return feeds
+
+		fmt.Printf("response: %s\n", b)
+
+		var f interface{}
+		if json.Unmarshal(b, &f) != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("f is %s\n\n", reflect.TypeOf(f))
+
+		// someone call Tom Hanks, we gunna cast away
+		feeds := f.([]interface {})
+		log.Println("Found", len(feeds), "feeds")
+		for _, feed_int := range feeds {
+			feed := feed_int.(map[string]interface{})
+			fmt.Printf("\t%s\n", feed["name"])
+			// delete(feed, "name")
+			for attr := range feed {
+				fmt.Printf("\t\t%s: %s\n", attr, feed[attr])			
+			} 
+			fmt.Println()
+		} 
 	}
 	return nil
 }
